56040047068ebe76466babbcc57dea984a8ee5e2
Paulo <paulohsilva2001@gmail.com>
2021-05-24 14:10:16 -0300

    Components

--- ProAgil-Web/src/app/app-routing.module.ts
+++ ProAgil-Web/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContatosComponent } from './contatos/contatos.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { PerfilComponent } from './perfil/perfil.component';
+
+const routes: Routes = [
+
+  { path: 'eventos', component: EventosComponent},
+  {path: 'palestrantes', component: PalestrantesComponent},
+  { path: 'contatos', component: ContatosComponent },
+  { path: 'perfil', component: PerfilComponent},
+
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
