21e1e8bd5a1736e20eaaec521c880b8c424c4603
Paulo <paulohsilva2001@gmail.com>
2021-05-24 14:10:34 -0300

    Linking routers

--- ProAgil-Web/src/app/app.module.ts
+++ ProAgil-Web/src/app/app.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import {  TooltipModule } from 'ngx-bootstrap/tooltip';
+import {  ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+
+
 import {HttpClientModule} from '@angular/common/http';
 
 
@@ -9,18 +17,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
+import { ContatosComponent } from './contatos/contatos.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [		
+  declarations: [					
     AppComponent,
       EventosComponent,
-      PalestrantesComponent
+      PalestrantesComponent,
+      NavComponent,
+      ContatosComponent,
+      PerfilComponent,
+      DashboardComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule, 
-    BrowserAnimationsModule
+    HttpClientModule,
+    ReactiveFormsModule, 
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    BsDatepickerModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
